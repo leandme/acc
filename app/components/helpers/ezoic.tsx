a356b8f216db1e959408caf9db1f0286da2e6be0
Matt Phelps <matt@leandme.com>
2026-03-02 21:30:41 +0100

    fd

--- app/components/helpers/ezoic.tsx
+++ app/components/helpers/ezoic.tsx
@@ -1,38 +1,32 @@
-import Script from "next/script";
-
 export function Ezoic() {
   return (
     <>
-      <Script
+      <script
         id="ezoic-privacy-min"
-        strategy="beforeInteractive"
         data-cfasync="false"
         src="https://cmp.gatekeeperconsent.com/min.js"
-      />
-      <Script
+      ></script>
+      <script
         id="ezoic-privacy-cmp"
-        strategy="beforeInteractive"
         data-cfasync="false"
         src="https://the.gatekeeperconsent.com/cmp.min.js"
-      />
-      <Script
+      ></script>
+      <script
         id="ezoic-header"
-        strategy="beforeInteractive"
-        src="https://www.ezojs.com/ezoic/sa.min.js"
-      />
-      <Script
+        async
+        src="//www.ezojs.com/ezoic/sa.min.js"
+      ></script>
+      <script
         id="ezoic-standalone-init"
-        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html:
             "window.ezstandalone = window.ezstandalone || {}; window.ezstandalone.cmd = window.ezstandalone.cmd || [];",
         }}
-      />
-      <Script
+      ></script>
+      <script
         id="ezoic-analytics"
-        strategy="beforeInteractive"
         src="https://ezoicanalytics.com/analytics.js"
-      />
+      ></script>
     </>
   );
 }
