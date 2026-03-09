f9a10e670768a19ab4d6310d9f0baddaae79e29d
Matt Phelps <matt@leandme.com>
2026-03-09 17:02:32 +0100

    df

--- app/components/common/footer.tsx
+++ app/components/common/footer.tsx
@@ -52,6 +52,7 @@ export default function Footer() {
               <li><a className="hover:text-white" href="/army-body-fat-calculator">Army Body Fat Calculator</a></li>
                <li><a className="hover:text-white" href="/ffmi-calculator">FFMI Calculator</a></li>
                <li><a className="hover:text-white" href="/bai-calculator">BAI Calculator</a></li>
+              <li><a className="hover:text-white" href="/tools">All Body Tools</a></li>
             </ul>
           </div>
 
@@ -65,6 +66,7 @@ export default function Footer() {
               <li><a className="hover:text-white" href="/intermittent-fasting-calculator">Intermittent Fasting Calculator</a></li>
               <li><a className="hover:text-white" href="/ideal-weight-calculator">Ideal Weight Calculator</a></li>
                <li><a className="hover:text-white" href="/ponderal-index-calculator">Ponderal Index Calculator</a></li>
+               <li><a className="hover:text-white" href="/tools/weight">All Weight Tools</a></li>
             </ul>
           </div>
 
@@ -80,6 +82,7 @@ export default function Footer() {
               <li><a className="hover:text-white" href="/attractiveness-test">Attractiveness Test</a></li>
               <li><a className="hover:text-white" href="/face-symmetry-test">Face Symmetry Test</a></li>
               <li><a className="hover:text-white" href="/golden-face-ratio-analyzer">Golden Face Ratio Analyzer</a></li>
+              <li><a className="hover:text-white" href="/tools/face">All Face Tools</a></li>
             </ul>
           </div>
 
@@ -94,6 +97,7 @@ export default function Footer() {
               <li><a className="hover:text-white" href="/steps-to-calories-calculator">Steps to Calories Calculator</a></li>
               <li><a className="hover:text-white" href="/calorie-deficit-calculator">Calorie Deficit Calculator</a></li>
               <li><a className="hover:text-white" href="/calories-burned-calculator">Calories Burned Calculator</a></li>
+              <li><a className="hover:text-white" href="/tools/calories">All Calorie Tools</a></li>
             </ul>
           </div>
 
@@ -103,7 +107,8 @@ export default function Footer() {
             </h6>
             <ul className="mt-3 space-y-2 text-base text-gray-300">
               <li><a className="hover:text-white" href="/height-estimator">Height Estimator</a></li>
-              <li><a className="hover:text-white" href="/height-estimator">Mid-Parental Height Calculator</a></li>
+              <li><a className="hover:text-white" href="/mid-parental-height-calculator">Mid-Parental Height Calculator</a></li>
+              <li><a className="hover:text-white" href="/tools/height">All Height Tools</a></li>
             </ul>
           </div>
 
